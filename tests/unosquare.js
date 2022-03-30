3fbd2a248d0ee13bf1c9666ea7c6c8aef3b4de98
Juan Davila <juan.davila@unosquare.com>
2022-03-30 10:36:38 -0600

    first pull request completed

--- tests/unosquare.js
+++ tests/unosquare.js
@@ -4,6 +4,13 @@ module.exports = {
       .windowMaximize()
       .url('https://www.unosquare.com')
       .waitForElementVisible('body')
+      .assert.attributeContains("a.navbar-brand img", "alt", "Unosqu")
+      .assert.attributeEquals("a.navbar-brand img", "alt", "Unosquare")
+      .verify.cssProperty(".navbar-brand", "height", "74px")
+      .assert.not.valueEquals(".navbar-brand", "X")
+      .assert.visible(".navbar-brand")
+      .assert.title("Digital Transformation Services | Agile Staffing Solutions | Unosquare")
+      .assert.urlContains("unosquare")
       .end();
     }
   };
